66ad602ac78a48a9da617bf48be3f8237e22350d
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-04-07 01:48:14 +0530

    added threads (recive and send) to support simultanious sendind and reciving to messages

--- client.py
+++ client.py
@@ -1,6 +1,90 @@
 import socket
 import os
 import subprocess
+from threading import *
+
+
+# "CONNECTED" IS USED TO CHECK IF CLIENT IS CONNECTED OR NOT
+global CONNECTED
+CONNECTED = True
+
+# DEFINATION OF CLASS "recive"
+class recive(Thread):
+
+        def run(self):
+
+                try:
+                        def reciving():
+                                
+                                while True:
+
+                                        # INNITIALLY "CONNECTED" IS TRUE
+                                        global CONNECTED
+
+                                        # RECIVES MESSAGES FROM SERVER ONLY IF CONNECTED
+                                        if CONNECTED:
+                                                msg = s.recv(1024)
+
+                                                # CLOSES THE CONNECTION IF "msg" IS "exit"
+                                                if msg.decode() == "exit":
+                                                        print("host exited")
+                                                        CONNECTED = False
+                                                else:
+                                                        print(msg.decode())
+
+                                        # BREAK OUT OF LOOP IF NOT CONNECTED TO SERVER
+                                        if not CONNECTED:
+                                             break
+                except:
+                        if CONNECTED:
+                                reciving()
+
+                # CALL "reciving" FUNCTION ONLY IF CONNECTED TO SERVER
+                if CONNECTED:
+                    reciving()
+
+# CREATING OBJECT FOR CLASS "recive"
+message_in = recive()
+
+
+# DEFINATION OF CLASS "send"
+class send(Thread):
+
+        def run(self):
+
+                try:
+                        def sending():
+                                while True:
+
+                                        # INNITIALLY "CONNECTED" IS TRUE
+                                        global CONNECTED
+
+                                        # CHEACKING IF CLIENT IS CONNECTED TO SERVER OR NOT
+                                        if CONNECTED == True:
+                                                reply = input(">>>")
+
+                                                # CLOSES THE CONNECTION IF "replay" IS "exit"
+                                                if reply == "exit":
+                                                        print("SUCCESSFULLY DIS-CONNECTED")
+                                                        s.send(reply.encode())
+                                                        CONNECTED = False
+                                                else:
+                                                        s.send(reply.encode())
+
+                                        # BREAK OUT OF LOOP IF NOT CONNECTED TO SERVER
+                                        if not CONNECTED:
+                                            break
+                except:
+                        if CONNECTED:
+                            sending()
+
+                # CALL "sending" FUNCTION ONLY IF CONNECTED TO SERVER
+                if CONNECTED:
+                        sending()
+
+# CREATING OBJECT FOR CLASS "send"
+message_out = send()
+
 
 # CLIENT WORKING IN ADVANCED MODE
 def advanced_mode():
@@ -30,48 +114,38 @@ def advanced_mode():
 
 # CLIENT WORKING IN NORMAL MODE
 def normal_mode():
+                print("operating in NORMAL MODE")
 
-        print("operating in NORMAL MODE")
+                # INITIATING "recive" THREAD
+                message_in.start()
 
-        while True:
-                msg = s.recv(1024)
+                # INITIATING "send" THREAD
+                message_out.start()
 
-                # CLOSES THE CONNECTION IF "msg" IS "exit"
-                if msg.decode() == "exit":
-                        print("host exited")
-                        break
-
-                print(msg.decode())
-                reply = input(">>>")
-
-                # CLOSES THE CONNECTION IF "reply" IS "exit"
-                if reply == "exit":
-                        print("SUCCESSFULLY DIS-CONNECTED")
-                        s.send(reply.encode())
-                        break
-                else:
-                        s.send(reply.encode())
 
 # STARTING CONNECTION WITH THE CLIENT
 def start_connection():
-    try:
-        global s
-        s = socket.socket()
-        host = "192.168.43.23"
-        port = 9999
+        try:
+                global s
+                s = socket.socket()
+                host = "192.168.43.23"
+                port = 9999
 
-        s.connect((host,port))
-    except:
-        print("trying to connect host....")
-        start_connection()
+                s.connect((host,port))
+        except:
+                print("trying to connect host....")
+                start_connection()
 
-    # opt  CONTAINS THE MODE OF WORKING
-    opt = s.recv(1024).decode()
+        # "opt"  CONTAINS THE MODE OF WORKING
+        opt = s.recv(1024).decode()
 
-    if opt == '1':
-        normal_mode()
-    if opt == '2':
-        advanced_mode()
+        # IF "opt" IS "1" INITIATE "normal mode"
+        if opt == '1':
+                normal_mode()
+
+        # IF "opt" IS "2" INITIATE "advanced mode"
+        if opt == '2':
+                advanced_mode()
 
 # INITIATING CONNECTION
 start_connection()
