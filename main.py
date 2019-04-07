8bad74644dfbb2115a8b43b1bb580765ad6a88ad
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-04-07 15:48:12 +0530

    changed main.py according to client.py and normal_mode.py

--- main.py
+++ main.py
@@ -1,19 +1,54 @@
+import server
 import normal_mode
 import advanced_mode
 
-# EXECUTION STRARTS FROM HEAR - main()
 class main():
-    while True:
-            opt = input("Menu:\n1.normal mode(chatting)\n2.advanved mode\n")
+        conn = ""
+        s = ""
 
-            # INITIATING NORMAL MODE
-            if opt == '1':
-                    normal = normal_mode.Mode(opt)
-                    normal.start()
+        # CREATING CONNECTION BETWEEN SERVER AND CLIENT
+        def start(self):
 
-            # INITIATING ADVANCED MODE
-            elif opt == '2':
-                    advanced = advanced_mode.Mode(opt)
-                    advanced.start()
-            else:
-                    print("invalid choice")
+                global conn,s
+                s = obj = server.networking()
+
+                # CREATING THE SOCKET
+                obj.create_socket()
+
+                # BINDING PORT AND PUTTING PORT TO LISTEN MODE
+                obj.bind_socket()
+
+                # ESTABLISHING CONNECTION WITH THE CLIENT
+                conn, adderss = obj.socket_accept()
+
+
+        # EXECUTION STRARTS FROM HEAR - main()
+        def StartMain(self):
+
+                global conn
+                opt = input("Menu:\n1.normal mode(chatting)\n2.advanved mode\n")
+
+                # INITIATING NORMAL MODE
+                if opt == '1':
+                        m.start()
+                        normal = normal_mode.Mode(opt,conn,s)
+                        normal.NormalStart()
+
+                # INITIATING ADVANCED MODE
+                elif opt == '2':
+                        m.start()
+                        advanced = advanced_mode.Mode(opt,conn)
+                        advanced.start()
+
+                else:
+                        print("invalid choice")
+
+'''================================================== STARTING MAIN ======================================================='''
+                        
+if __name__ == "__main__":
+
+        # CREATING OBJECT OF CLASS "main"
+        m = main()
+
+        # INITIATING "StartMain"
+        m.StartMain()
