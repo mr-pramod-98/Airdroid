9aa2e66f7049fbb4e4dbb121cefd4062ca93fa68
mr-pramod-98 <43513968+mr-pramod-98@users.noreply.github.com>
2019-07-12 19:04:15 +0530

    fix bugs and improved coding

--- ShareZone.py
+++ ShareZone.py
@@ -1,120 +1,210 @@
 from threading import *
 from time import sleep
 
-# "NAME" IS USED TO REFER SERVER
-NAME = "HOST"
 
-# "CONNECTED" IS USED TO CHECK IF CLIENT IS CONNECTED OR NOT
-CONNECTED = True
+# "CONNECTED" IS USED TO CHECK IF CLIENT IS CONNECTED OR NOT.
 
-# "all_connections" IS USED HOLD ALL THE CONNECTED OBJECTS
+# "n" IS NUMBER OF CONNECTIONS.
+n = 2
+
+# "all_connections" IS USED HOLD ALL THE CONNECTED OBJECTS.
 all_connections = []
 
-# "index" IS USED TO ACCESS ELEMENTS TO THE LIST "conn"
+# "index" IS USED TO ACCESS ELEMENTS TO THE LIST "conn".
 index = None
 
 option = None
 
-# DEFINATION OF CLASS "Mode"
+
+# DEFINITION OF CLASS "Mode".
 class Mode():
 
-        # INNITIALIZING "n"(number of connections) and "all_connections"
-        def __init__(self, num, opt, conn_obj):
 
-                global all_connections, n, option
-                n = num
-                all_connections = conn_obj
-                option = opt
+    # INITIALIZING "n"(number of connections) and "all_connections".
+    def __init__(self, num, opt, conn_obj):
 
-        '''========================================= START OF GROUP MODE ==========================================='''
+        global all_connections, n, option
+        n = num
+        all_connections = conn_obj
+        option = opt
 
-        # STARTING GROUP MODE
-        def GroupStart(self):
+    '''========================================= START OF GROUP MODE ==========================================='''
 
-                '''''========================================= THREAD RECIVE ==========================================='''''
+    # STARTING GROUP MODE
+    def GroupStart(self):
 
-                # DEFINATION OF CLASS "recive"
-                class coordinate(Thread):
+        '''''========================================= THREAD COORDINATE ==========================================='''''
 
-                        def run(self):
+        # DEFINITION OF CLASS "receive"
+        class Co_ordinate(Thread):
 
-                                global index
-                                i = index
+            def run(self):
+
+                global index
+                i = index
+                CONNECTED = True
+
+                try:
+                    def receiving():
+
+                        while True:
+
+                            # INITIALLY "CONNECTED" IS TRUE
+                            global all_connections, n
+                            CONNECTED = True
+
+                            # RECEIVES MESSAGES FROM SERVER ONLY IF CONNECTED
+                            if CONNECTED:
 
                                 try:
-                                        def reciving():
-                                                while True:
+                                    # COLLECTING THE RESPONSE OF 'i'th OBJECT IN THE LIST "all_connections"
+                                    response = all_connections[i].recv(2048)
+                                    response = response.decode()
 
-                                                        # INNITIALLY "CONNECTED" IS TRUE
-                                                        global CONNECTED, all_connections
+                                    # CLOSES THE CONNECTION IF RESPONSE IS "exit"
+                                    if 'exit' in response and '>>' not in response:
 
-                                                        # RECIVES MESSAGES FROM SERVER ONLY IF CONNECTED
-                                                        if len(all_connections) > 0:
-                                                                try:
-                                                                        # COLLECTING THE RESPONSE OF 'i'th OBJECT IN THE LIST "all_connections"
-                                                                        response = all_connections[i].recv(2048)
+                                        # CLOSE THE CONNECTION AND DECREMENT "n"
+                                        all_connections[i].close()
+                                        n = n - 1
+                                        CONNECTED = False
 
-                                                                        # CLOSES THE CONNECTION IF RESPONSE IS "exit"
-                                                                        if 'exit' in response.decode() and '>>' not in response.decode():
-                                                                                all_connections[i].close()
-                                                                                all_connections.remove(all_connections[i])
+                                        # GENERATING A FRIENDLY MESSAGE TO BE SENT
+                                        response = response.replace("exit", "")
+                                        response = response + "left the connection, Clients left - " + str(n)
 
-                                                                                # INTIMATING TO OTHER CONNECTED CLIENT THAT "all_connectios[i]" AS LEFT THE CONVERSATION
-                                                                                response = response.decode() + "ed" + " Clients left - " + str(len(all_connections))
-                                                                                for c in all_connections:
-                                                                                        c.send(response.encode())
+                                        # INTIMATING TO OTHER CONNECTED CLIENT THAT "all_connectios[i]" AS LEFT THE CONVERSATION.
+                                        # THE EXCEPTION OF SENDING MESSAGE TO A DIS-CONNECTED IS HANDLED BY THE try AND except BLOCK.
+                                        for c in all_connections:
+                                            try:
+                                                c.send(response.encode())
+                                            except:
+                                                pass
 
-                                                                        else:
-                                                                                response = response.decode()
+                                    elif "/FILE/" in response:
 
-                                                                                # SENDING RESPONSE TO ALL CONNECTED CLIENTS EXCEPT TO THE CLIENT WHICH IS GENERATING THE RESPONSE
-                                                                                for c in all_connections:
+                                        # SENDING REQUEST TO ALL CONNECTED CLIENTS EXCEPT TO THE CLIENT WHICH IS GENERATING THE REQUEST
+                                        for c in all_connections:
 
-                                                                                        # "all_connections[i]" IS GENERATING THE RESPONSE
-                                                                                        if c == all_connections[i]:
-                                                                                                continue
-                                                                                        c.send(response.encode())
-                                                                except:
-                                                                        pass
+                                            # "all_connections[i]" IS GENERATING THE REQUEST
+                                            # THE EXCEPTION OF SENDING MESSAGE TO A DIS-CONNECTED IS HANDLED BY THE try AND except BLOCK.
+                                            if c == all_connections[i]:
+                                                continue
+                                            try:
+                                                c.send(response.encode())
+                                            except:
+                                                pass
 
-                                                        # BREAK OUT OF LOOP IF NOT CONNECTED TO SERVER
-                                                        if not (len(all_connections) > 0):
-                                                                break
+                                        try:
+                                            path = response.replace("/FILE/", "")
+                                            print("path :", path)
+                                            file = open(path, 'rb')
+
+                                            # SENDING DATA OF THE FILE IN A LOOP
+                                            for data in file:
+
+                                                # SENDING data TO ALL OTHER CONNECTED CLIENTS
+                                                for c in all_connections:
+
+                                                    # "all_connections[i]" IS GENERATING THE REQUEST
+                                                    # THE EXCEPTION OF SENDING MESSAGE TO A DIS-CONNECTED IS HANDLED BY THE try AND except BLOCK.
+                                                    if c == all_connections[i]:
+                                                        continue
+                                                    try:
+                                                        c.send(data)
+                                                    except:
+                                                        pass
+
+                                            # SENDING "FILE EXIT"  MESSAGE TO ALL CONNECTED CLIENTS EXCEPT TO THE CLIENT WHICH IS SENDING THE FILE
+                                            for c in all_connections:
+
+                                                # "all_connections[i]" IS SENDING THE FILE
+                                                if c == all_connections[i]:
+                                                    continue
+                                                try:
+                                                    c.send(bytes("FILE EXIT".encode()))
+                                                except:
+                                                    pass
+
+                                            msg_complete = "file sent"
+                                            all_connections[i].send(msg_complete.encode())
+                                            file.close()
+
+                                        except:
+
+                                            # NOTIFYING SENDER THAT ERROR HAS OCCURRED
+                                            msg_exception = "file could not be transfered pleas try again"
+                                            all_connections[i].send(msg_exception.encode())
+
+                                            # SENDING "FILE EXIT"  MESSAGE TO ALL CONNECTED CLIENTS EXCEPT TO THE CLIENT WHICH IS SENDING THE FILE
+                                            for c in all_connections:
+
+                                                # "all_connections[i]" IS SENDING THE FILE
+                                                # THE EXCEPTION OF SENDING MESSAGE TO A DIS-CONNECTED IS HANDLED BY THE try AND except BLOCK.
+                                                if c == all_connections[i]:
+                                                    continue
+                                                try:
+                                                    c.send(bytes("TRANSFERED FILED".encode()))
+                                                    c.send(bytes("FILE EXIT".encode()))
+                                                except:
+                                                    pass
+
+                                    else:
+
+                                        # SENDING RESPONSE TO ALL CONNECTED CLIENTS EXCEPT TO THE CLIENT WHICH IS GENERATING THE RESPONSE
+                                        for c in all_connections:
+
+                                            # "all_connections[i]" IS GENERATING THE RESPONSE
+                                            # THE EXCEPTION OF SENDING MESSAGE TO A DIS-CONNECTED IS HANDLED BY THE try AND except BLOCK.
+                                            if c == all_connections[i]:
+                                                continue
+                                            try:
+                                                c.send(response.encode())
+                                            except:
+                                                pass
 
                                 except:
-                                        if len(all_connections) > 0:
-                                                reciving()
+                                    print("error")
+                                    pass
 
-                                # CALL "reciving" FUNCTION ONLY IF CONNECTED TO SERVER
-                                if len(all_connections) > 0:
-                                        reciving()
+                            # BREAK OUT OF LOOP IF NOT CONNECTED TO SERVER
+                            if not CONNECTED:
+                                break
 
-                # "message_in" LIST IS USED TO HOLD THE OBJECTS OF CLASS "recive"
-                message_in = []
+                except:
+                    if CONNECTED:
+                        receiving()
 
-                # CREATING "n" OBJECTS FOR CLASS "recive"
-                for i in range(n):
-                        obj = coordinate()
-                        message_in.append(obj)
+                # CALL "receiving" FUNCTION ONLY IF CONNECTED TO CLIENT
+                if CONNECTED:
+                    receiving()
 
-                '''======================================== SENDING MESSAGES ========================================'''
+        # "message_in" LIST IS USED TO HOLD THE OBJECTS OF CLASS "receive"
+        message_in = []
 
-                # SENDING MESSAGES TO CLIENTS
-                def send_messages():
+        # CREATING "n" OBJECTS FOR CLASS "receive"
+        for i in range(n):
+            obj = Co_ordinate()
+            message_in.append(obj)
 
-                        # SETTING INITIAL LOOK AND INITIAL CONDITION
-                        print("\n============================================ OPERATING IN GROUP MODE ==========================================")
-                        print("                         ************************ READY TO USE ************************                        \n")
+        '''======================================== SENDING MESSAGES ========================================'''
 
-                        for c in all_connections:
-                                c.send(option.encode())
+        # SENDING MESSAGES TO CLIENTS
+        def send_messages():
 
-                        # INITIATING THREAD RECIVE FOR ALL THE OBJECT'S IN THE LIST "message_in"
-                        for i in range(n):
-                                global index
-                                index = i
-                                message_in[i].start()
-                                sleep(1)
+            # SETTING INITIAL LOOK AND INITIAL CONDITION
+            print("\n============================================ OPERATING IN GROUP MODE ==========================================")
+            print("                         ************************ READY TO USE ************************                        \n")
 
-                # INITIATING "send_message" FUNCTION
-                send_messages()
+            for c in all_connections:
+                c.send(option.encode())
+
+            # INITIATING THREAD RECEIVE FOR ALL THE OBJECT'S IN THE LIST "message_in"
+            for i in range(n):
+                global index
+                index = i
+                message_in[i].start()
+                sleep(1)
+
+        # INITIATING "send_message" FUNCTION
+        send_messages()
